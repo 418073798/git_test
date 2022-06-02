d0b26d67033f26171a814569f1a27bed79e42ca0
sjk <62605473+418073798@users.noreply.github.com>
2022-06-02 10:14:31 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("push commit222");
         System.out.println("push ssh");
+        System.out.println("pull ssh");
     }
 
 }
