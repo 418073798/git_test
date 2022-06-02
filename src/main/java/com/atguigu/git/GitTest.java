0fa1cb95650173d9f57443aba6590b26ed72c584
sjk <sjk@123.com>
2022-06-02 09:26:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author sjk
+ * @create 2022-06-02 8:52
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
